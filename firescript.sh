a6874cb97ab9f3759d9b4677067f898f5d5ca44b
Bitsz505 <105164741+Bitsz505@users.noreply.github.com>
2026-01-22 21:54:21 -0600

    Update menu options and add script calls
    Added options for software deletion, tcp cookies, ufw, ssh, and password policy

--- firescript.sh
+++ firescript.sh
@@ -8,4 +8,68 @@ fi
 
 #---Configuration---
 PS3='Select a task(1-8): '
-options=("Delete software" "Disable SSH" "Enable TCP Cookies" "Enable UFW" "Enact W Policies" "Update System" "Check for Viruses" "Quit")
+options=("Delete Software" "Disable SSH" "Enable TCP Cookies" "Enable UFW" "Enact W Policies" "Update System" "Check for Viruses" "Quit")
+
+#---Menu Loop---
+clear
+echo"==============================="
+echo"     LINUX MANAGEMENT MENU     "
+echo"==============================="
+
+select opt in "${option[@]}"
+do
+  case $opt in
+    "Delete Software")
+      dos2unix bulk_delete_software.sh
+      chmod +x bulk_delete_software.sh
+      read -p "Enter package name(s) to delete (space seperated):  " pkgs
+      # Calling the deletion script
+      ./bulk_delete_software.sh $pkgs
+      echo "Press Enter to return to menu..."
+      read 
+      ;;
+
+    "Disable SSH")
+      dos2unix disable_ssh.sh
+      chmod +x disable_ssh.sh
+      echo "Disabling SSH Service.."
+      # Calling SSH script
+      ./disable_ssh.sh
+      echo "Press Enter to return to menu..."
+      read 
+      ;;
+
+    "Enable TCP Cookies")
+      dos2unix enable_tcp_cookies.sh
+      chmod +x enable_tcp_cookies.sh
+      echo "Enabling TCP Cookies..."
+      # Calling Cookie script
+      ./enable_tcp_cookies.sh
+      echo "Press Enter to return to menu..."
+      read 
+      ;;
+
+    "Enable UFW")
+      dos2unix enable_ufw.sh
+      chmod +x enable_ufw.sh
+      echo "Enabling UFW..."
+      # Calling UFW Script
+      ./enable_ufw.sh
+      echo "Press Enter to return to menu..."
+      read 
+      ;;
+
+    "Enact W Policies")
+      dos2unix good_policies.sh
+      chmod +x good_policies.sh
+      read -p "Enter desired minimum length (e.g., 120: " passlen
+      # Calling the script with the input as an argument
+      ./good_policies.sh "$passlen"
+      echo "Press Enter to return to menu..."
+      read 
+      ;;
+
+        
+        
+      
+      
