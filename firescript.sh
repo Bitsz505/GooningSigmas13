2f7ad1d23036e8d731d3cf03e57a1b3088cda2f8
Bitsz505 <105164741+Bitsz505@users.noreply.github.com>
2026-01-22 21:01:57 -0600

    Implement root user check in firescript.sh
    Added root user check to ensure script runs with sudo.
    Started working on menu

--- firescript.sh
+++ firescript.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 #---Check for root---
+if ["$EUID" -ne 0]; then 
+  echo "Please run this menu with sudo: sudo $0"
+  exit 1
+fi
+
+#---Configuration---
+PS3='Select a task(1-8): '
+options=("Delete software" "Disable SSH" "Enable TCP Cookies" "Enable UFW" "Enact W Policies" "Update System" "Check for Viruses" "Quit")
