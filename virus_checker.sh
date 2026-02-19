4fcbd10b1f5872cdd16a37dbbc173b71805e56bd
Bitsz505 <105164741+Bitsz505@users.noreply.github.com>
2026-02-19 15:27:52 -0600

    Enhance virus_checker.sh with checks and output files
    Added root user check and output files for scan results.

--- virus_checker.sh
+++ virus_checker.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-sudo apt install clamav
-clamscan
+if ["$EUID" -ne 0 ]; then
+	echo "Error: This script must be run as root (use sudo)."
+	exit 1
+fi
+touch clamscanoutput.txt
+touch rootkitoutput.txt
 
-sudo apt install rkhunter
-rkhunter --check
+apt install clamav
+clamscan > clamscanoutput.txt
+
+apt install chrootkit
+chrootkit -q > rootkitoutput.txt
+
+grep 'FOUND' clamscanoutput.txt
+
 
